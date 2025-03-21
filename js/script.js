def4968c5f2590f693f6760c0b1df5b500800cdd
Nauka <berdinauryzbek@gmail.com>
2025-03-20 21:15:51 -0400

    updated navbar

--- js/script.js
+++ js/script.js
@@ -2,20 +2,22 @@
 
 document.title = 'Power Gym';
 
-const logoTitleContainer = document.querySelector('.logo-title-container').innerHTML = `
-  <img class="dumbbell-logo" src="../icons/dumbbell.png" alt="Dumbbell">
-  <h3><a class="title" href="../index.html">POWER GYM</a></h3>
-`;
-const nav = document.querySelector('nav').innerHTML = `
-  <ul class="nav justify-content-center">
-    <li class="nav-item">
-      <a class="nav-link active" aria-current="page" href="./nav/aboutus.html">ABOUT US</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="./nav/contact.html">Contact Us</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="./nav/help.html">Help</a>
-    </li>
-  </ul>
+document.querySelector('header').innerHTML = `
+  <div class="logo-title-container">
+    <img class="dumbbell-logo" src="../icons/dumbbell.png" alt="Dumbbell">
+    <h3><a class="title" href="../index.html">POWER GYM</a></h3>
+  </div>
+  <nav>
+    <ul class="nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="./nav/aboutus.html">ABOUT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./nav/contact.html">Contact Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./nav/help.html">Help</a>
+      </li>
+    </ul>
+  </nav>
 `;
